c13ca7c8e502f50240762750ee38b2e667ae701e
Melvin Flores <flores.majano@gmail.com>
2018-12-18 08:40:07 -0600

    Ejercicio realizado en MVC

---
+++ AppMultiplicar/Controllers/MultiController.cs
@@ -0,0 +1,19 @@
+﻿using AppMultiplicar.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppMultiplicar.Controllers
+{
+public class MultiController : Controller
+{
+calculos Modelo = new calculos();
+public ActionResult Index(){
+ViewBag.bandera = false;
+if (Request.HttpMethod == "POST"){
+ViewBag.x = true;int nuevo = Convert.ToInt32(Request["txtTabla"]);
+ViewBag.ver = Modelo.Proceso(nuevo);}
+return View();
+}}}
---
+++ AppMultiplicar/Models/calculos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppMultiplicar.Models
+{
+public class calculos
+{
+public List<string> Proceso(int n)
+{
+List<string> t = new List<string>();
+int c = 1;
+while (c <= 10)
+{
+string cadena;
+cadena = n + "X" + c + " = " + (n*c);
+t.Add(cadena);
+c += 1;
+}
+return t; 
+}
+}
+}
